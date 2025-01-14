f9712bc1d24c8c5b5ede2cb8e99a2dc2166b1cc3
owenbcoding <eoghankb@gmail.com>
2025-01-14 16:46:24 +0000

    created header component and added hero image

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<div>
+    <!-- Hero card -->
+    <div class="relative">
+        <div class="absolute inset-x-0 bottom-0 h-full"></div>
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+                <div class="absolute inset-0">
+                    <img class="h-full w-full object-cover" src="../images/hero-image.png"
+                        alt="People working on laptops">
+                    <div class="absolute inset-0 bg-gray-200 mix-blend-multiply"></div>
+                </div>
+                <div class="relative py-16 px-6 sm:py-24 lg:py-32 lg:px-8">
+                    <h1 class="text-center text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
+                        <span class="block font-medium text-gray-200">Blog app</span>
+                        <div class="absolute inset-0 bg-gray-200 mix-blend-multiply"></div>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

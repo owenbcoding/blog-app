43c861ba7d24fbd027ddb2bdbfe94b052ba7ca89
owenbcoding <eoghankb@gmail.com>
2025-01-15 18:35:26 +0000

    removed h1 and commented out posts view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
     <x-header />
-    <div>
-        <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Latest blogs</h1>
-    </div>
+    {{-- <x-posts /> --}}
 </x-layout>

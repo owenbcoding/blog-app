2e7fb83970a5ddc27b7356cb7e0acd4ec895f5a6
owenbcoding <eoghankb@gmail.com>
2025-01-15 19:07:12 +0000

    Add blog listing section with posts component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
     <x-header />
-    {{-- <x-posts /> --}}
+    <div class="container mx-auto mt-10">
+        <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Latest blogs
+        </h1>
+        <x-posts :posts="$posts" />
+    </div>
 </x-layout>

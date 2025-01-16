cdd6a5d3bd0da0d06ff21acae8d85f63bb8adce4
owenbcoding <eoghankb@gmail.com>
2025-01-16 12:50:54 +0000

    created routes for about and contact pages

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,11 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
+Route::get('/about', function () {  
+    return view('about');   })->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
 
 require __DIR__.'/auth.php';

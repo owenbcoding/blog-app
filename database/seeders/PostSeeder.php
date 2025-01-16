6cca17a677e906cf181815166fc36162cda53e23
owenbcoding <eoghankb@gmail.com>
2025-01-16 14:23:58 +0000

    removed comment for post::factory create

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = Tag::factory(3)->create();
+
+        Post::factory(6)->create();
+    }
+}

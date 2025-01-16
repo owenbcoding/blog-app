41843b28e2c356012db9dc6d98fd66eed15a4465
owenbcoding <eoghankb@gmail.com>
2025-01-16 11:44:02 +0000

    created default post image component

---
+++ resources/views/components/post-image.blade.php
@@ -0,0 +1,3 @@
+@props(['width' => 600, 'height' => 400, 'seed'])
+
+<img src="https://picsum.photos/seed/{{ $seed }}/{{ $width }}" alt="project-image" class="aspect-[16/9] w-full rounded-t-lg bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">

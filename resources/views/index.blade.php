e2a7554f1121ed954949fd4275fedd7accebe229
owenbcoding <eoghankb@gmail.com>
2025-01-16 14:39:00 +0000

    added some margin top to posts layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,8 @@
     <div class="container mx-auto mt-10">
         <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Latest blogs
         </h1>
-        <x-posts :posts="$posts" />
+        <div class="mt-10">
+            <x-posts :posts="$posts" />
+        </div>
     </div>
 </x-layout>

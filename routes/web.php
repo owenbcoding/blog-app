e0632b432d4e57393764f92c0be191a51c3eb0d9
owenbcoding <eoghankb@gmail.com>
2025-01-16 15:54:43 +0000

    formatted routes to be cleaner

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/about', function () {  
-    return view('about');   })->name('about');
+    return view('about'); 
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
 
 require __DIR__.'/auth.php';

433af3f66a818aaff0774779f3ab3ed512285870
owenbcoding <eoghankb@gmail.com>
2025-01-17 16:08:42 +0000

    created show page for blog posts

---
+++ resources/views/show.blade.php
@@ -0,0 +1,29 @@
+<x-layout>
+    <x-header />
+    <div class="dark:text-white py-10 px-6 lg:px-8">
+        <canvas id="canvas" class="hidden absolute opacity-75 bg-gray-700"></canvas>
+        <div class="mx-auto max-w-3xl text-base leading-7">
+            <h1 class="dark:text-gray-400 text-3xl font-bold tracking-tight sm:text-4xl">{{ $post->title }}</h1>
+            <p class="text-xl leading-8 mt-5 mb-5">{{ $post->excerpt }}</p>
+            <div class="mt-5 mb-5">
+                @if ($post->tags && $post->tags->count())
+                    @foreach ($post->tags as $tag)
+                        <a href="/categories/{{ $tag->slug }}"
+                            class="relative z-10 rounded-lg bg-gray-800 py-2 px-3 font-medium text-white hover:bg-gray-100">{{ $tag->name }}</a>
+                    @endforeach
+                @endif
+            </div>
+            <figure>
+                @if ($post->thumbnail)
+                    <img class="aspect-video rounded-xl bg-gray-50 object-cover"
+                        src="{{ asset('storage/' . $post->thumbnail) }}" alt="post-image">
+                @else
+                    <x-post-card-image :seed="$post->id" />
+                @endif
+            </figure>
+            <div class="mt-5 max-w-2xl dark:text-white">
+                <p class="mt-6 text-lg">{{ $post->body }}</p>
+            </div>
+        </div>
+    </div>
+</x-layout>

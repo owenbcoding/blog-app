be65a8ba46a53d5a79f9643d9305e0584f149282
owenbcoding <eoghankb@gmail.com>
2025-01-18 21:10:08 +0000

    renamed component from x-layout to layout.app

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layouts.app>
     <x-header />
     <div class="container mx-auto mt-10">
         <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Latest blogs
@@ -7,4 +7,4 @@
             <x-posts :posts="$posts" />
         </div>
     </div>
-</x-layout>
+</x-layouts.app>

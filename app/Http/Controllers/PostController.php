eaa016d0adcdb07f93da8ef0993208aea1020aa3
owenbcoding <eoghankb@gmail.com>
2025-01-18 21:07:26 +0000

    testing different logic for displaying blogs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
@@ -10,9 +11,14 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('published_at')->latest()->paginate(6);
-        return view('index', compact('posts'));
+        // $posts = Post::where('published_at')->latest()->paginate(6);
+        // $posts = Post::where("published_at", "!=", null)->latest()->with('tags')->paginate(6);
+        return view('index', [
+            'posts' => Post::where("published_at")->latest()->paginate(6)
+        ]);
+        // return view('index', compact('posts'));
     }
+
 
     public function show(Post $post)
     {

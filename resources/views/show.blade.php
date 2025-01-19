03452b17ce8d2e879f853a57cc2b0286b8e84216
owenbcoding <eoghankb@gmail.com>
2025-01-19 20:39:38 +0000

    renamed show page to results

--- resources/views/show.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-header />
-    <div class="container mx-auto mt-10">
-        <h1 class="text-2xl font-bold mb-4">{{ $post->title }}</h1>
-        <p class="text-gray-600">{{ $post->body }}</p>
-    </div>
-</x-layout>

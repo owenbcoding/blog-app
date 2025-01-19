a4656409fec917dd6db3f4067f0c57df09c3d99b
owenbcoding <eoghankb@gmail.com>
2025-01-19 20:27:55 +0000

    refactored posts component to use post card component instead

--- resources/views/components/posts.blade.php
+++ resources/views/components/posts.blade.php
@@ -4,10 +4,7 @@
     @if ($posts->count() > 0)
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             @foreach ($posts as $post)
-                <div class="border rounded p-4">
-                    <h2 class="text-xl font-semibold">{{ $post->title }}</h2>
-                    <p>{{ $post->excerpt }}</p>
-                </div>
+                <x-post-card :post="$post" />
             @endforeach
         </div>
         <div class="flex justify-center mt-4">

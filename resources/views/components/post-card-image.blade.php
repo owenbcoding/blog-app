1a9b4e008844597d1d14c590fe660fbc614edc2a
owenbcoding <eoghankb@gmail.com>
2025-01-21 18:28:09 +0000

    removed height from props

--- resources/views/components/post-card-image.blade.php
+++ resources/views/components/post-card-image.blade.php
@@ -1,3 +1,3 @@
-@props(['width' => 600, 'height' => 400, 'seed'])
+@props(['width' => 600, 'seed'])
 
-<img src="https://picsum.photos/seed/{{ $seed }}/{{ $width }}" alt="blog-image" class="aspect-[16/9] w-full rounded-t-lg bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
+<img src="https://picsum.photos/seed/{{ $seed }}/{{ $width }}" alt="blog-image" class="aspect-[16/9] h-52 w-96 rounded-xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">

7c17239ac25825a94758caf32f3c65ea8ccf950e
owenbcoding <eoghankb@gmail.com>
2025-01-21 18:47:56 +0000

    Updated styles for image and remote post in for each for tags

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -5,7 +5,7 @@
         <div class="relative">
             @if ($post->thumbnail)
                 <img src="{{ asset('storage/' . $post->thumbnail) }}"
-                    alt="blog-image" class="aspect-[16/9] rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
+                    alt="blog-image" class="aspect-[16/9] rounded-full bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]">
             @else
                 <x-post-card-image :seed="$post->id" />
             @endif
@@ -15,7 +15,7 @@
             <div class="mt-8 flex items-center gap-x-4 text-xs">
                 <time class="text-gray-500">{{ $post->created_at->diffForHumans() }}</time>
                 @if ($post->tags && $post->tags->count())
-                    @foreach ($post->tags as $tag)
+                    @foreach ($tags as $tag)
                         <p class="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100">{{ $tag->name }}</p>
                     @endforeach
                 @endif

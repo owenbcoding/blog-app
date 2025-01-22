81164afaafb6b1ab7b1e60249cb8e11f6ce20c72
owenbcoding <eoghankb@gmail.com>
2025-01-22 20:13:11 +0000

    commented out route for post controller post slug for now and added route for admin main page index view with middleware

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
+// Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
+
+// Route::get('/posts/{post:slug}', function (\App\Models\Post $post) {
+//     return view('post', ['post' => $post]);
+// })->name('posts.show');
+
 Route::get('/about', function () {  
     return view('about'); 
 })->name('about');
@@ -13,6 +19,9 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/admin', function (){
+    return view('admin.index');
+})->name('admin.index')->middleware("auth");
+
 
 require __DIR__.'/auth.php';

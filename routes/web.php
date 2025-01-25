abb26614613b8a86574c45f3fa09a40d61ae571b
owenbcoding <eoghankb@gmail.com>
2025-01-25 22:09:07 +0000

    commtend out admin route

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-Route::get('/admin', function (){
-    return view('admin.index');
-})->name('admin.index')->middleware("auth");
+// Route::get('/admin', function (){
+//     return view('admin.index');
+// })->name('admin.index')->middleware("auth");
 
 Route::get('/dashboard', function () { 
     return view('dashboard');
@@ -32,6 +32,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-}); 
+});
 
 require __DIR__.'/auth.php';

7aea7e68f7d2a2167fb27e9ce373fc7404da745a
owenbcoding <eoghankb@gmail.com>
2025-01-25 21:39:50 +0000

    re-added routes for dashboard and profile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
@@ -23,5 +24,14 @@ Route::get('/admin', function (){
     return view('admin.index');
 })->name('admin.index')->middleware("auth");
 
+Route::get('/dashboard', function () { 
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+ 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+}); 
 
 require __DIR__.'/auth.php';

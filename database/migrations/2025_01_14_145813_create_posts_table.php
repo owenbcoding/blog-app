d807c83bd700722d0d6915e19cc9baf591119187
owenbcoding <eoghankb@gmail.com>
2025-01-26 01:28:33 +0000

    removed comented out slug and tag columns in posts migration

--- database/migrations/2025_01_14_145813_create_posts_table.php
+++ database/migrations/2025_01_14_145813_create_posts_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            // $table->string('slug');
             $table->string('title');
-            // $table->string('tag');
             $table->string('thumbnail')->nullable();
             $table->text('excerpt');
             $table->longText('body')->nullable();

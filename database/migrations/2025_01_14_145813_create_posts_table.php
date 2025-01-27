0a03e780c8ced8e92c1cb6492cb5f94178d2492c
owenbcoding <eoghankb@gmail.com>
2025-01-27 10:36:21 +0000

    add column for url to posts migration

--- database/migrations/2025_01_14_145813_create_posts_table.php
+++ database/migrations/2025_01_14_145813_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('excerpt');
             $table->longText('body')->nullable();
+            $table->string('url');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });

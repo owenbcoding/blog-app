3b5ef39fa0b3c148460cb4e1d60ee1984ade7f1c
owenbcoding <eoghankb@gmail.com>
2025-01-27 10:34:16 +0000

    Add TagController to handle post tag retrieval and pass data to post-card view

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
+use App\Models\Post;
 
 class TagController extends Controller
 {
-    
+    public function __invoke(Post $post)
+    {
+        $tags = $post->tags;
+        return view('post-card', ['post' => $post, 'tags' => $tags]);
+    }
+    // return view('show', 'posts' => $tag->posts);
 }

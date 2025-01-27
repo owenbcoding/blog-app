39188fc9f1168d85722590926aabbbb6b433c7ad
owenbcoding <eoghankb@gmail.com>
2025-01-27 12:39:35 +0000

    Remove unnecessary tags binding from post card component

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -4,7 +4,7 @@
     <div class="md:grid md:grid-cols-2 lg:grid-cols-3 sm:grid">
         <!--Grid with 3 blog posts-->
         @foreach ($posts->skip(0) as $post)
-            <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" :tags="$post->tags" />
+            <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"  />
         @endforeach
     </div>
 @endif

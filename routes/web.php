df5ce6a5d6af396f6e858f5570f92367dc865806
owenbcoding <eoghankb@gmail.com>
2025-01-29 17:36:34 +0000

    created route to display show post page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\TagController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('/tags/{tag:name}', TagController::class);
 // Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');

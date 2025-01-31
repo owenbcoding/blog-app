035e158f104d03ca60e43e3034555d62500aec6c
owenbcoding <eoghankb@gmail.com>
2025-01-31 14:14:22 +0000

    refactored posts index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,19 @@
 <x-layouts.app>
     <x-header />
     <div class="space-y-10">
-        <section> 
-            <div class="grid grid-cols-3 gap-8 max-w-7xl mx-auto mt-6">
-                @foreach ($posts as $post)
-                    <x-post-card :post="$post" />
-                @endforeach
-            </div>
+        <section>
+                @if ($posts->count() > 0)
+                    <div class="grid grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto lg:grid-cols-3 gap-6">
+                        @foreach ($posts as $post)
+                            <x-post-card :post="$post" />
+                        @endforeach
+                    </div>
+                    <div class="flex justify-center mt-4">
+                        {{ $posts->links() }}
+                    </div>
+                @else
+                    <p class="text-center mt-20">No posts yet. Please check back later.</p>
+                @endif
         </section>
     </div>
 </x-layouts.app>

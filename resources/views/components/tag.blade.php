c770413d013a18319030b947f8997366d91c2dc9
owenbcoding <eoghankb@gmail.com>
2025-02-01 02:50:53 +0000

    modified styles for tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,5 +1,5 @@
 @props(['tag', 'size' => 'small'])
 
-<span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
+<span class="inline-flex items-center px-5 py-2 rounded-md text-sm font-medium bg-blue-100 text-blue-800">
     {{ $tag->name }}
 </span>

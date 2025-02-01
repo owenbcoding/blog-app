98b41f2afd2983be09627112e23421caec6ace34
owenbcoding <eoghankb@gmail.com>
2025-02-01 02:45:31 +0000

    add protected model user::class

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -16,6 +17,8 @@ class UserFactory extends Factory
      */
     protected static ?string $password;
 
+    protected $model = User::class;
+    
     /**
      * Define the model's default state.
      *

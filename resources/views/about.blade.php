9dccb57e6394ce0c4bf6a2625e16ad46de12234d
owenbcoding <eoghankb@gmail.com>
2025-02-01 02:49:47 +0000

    add .app to x-layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
-<x-layout>
+<x-layouts.app>
     <x-header />
     <div class="container mx-auto mt-10">
         <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">About
         </h1>
     </div>
-</x-layout>
+</x-layouts.app>

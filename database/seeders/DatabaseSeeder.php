bc9f796f26de7108e8a78c0b5cf4558ed453990b
owenbcoding <eoghankb@gmail.com>
2025-02-01 02:46:19 +0000

    modified create user and added post factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use \App\Models\Post;
 use Illuminate\Database\Seeder;
 use Database\Seeders\PostSeeder;
+use Database\Seeders\AdminSeeder;
+use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
@@ -16,10 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        $user = user::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'is_admin' => true,
         ]);
+
+        Post::factory(3)->create(['user_id' => $user->id]);
 
         $this->call(PostSeeder::class);
         $this->call(AdminSeeder::class);

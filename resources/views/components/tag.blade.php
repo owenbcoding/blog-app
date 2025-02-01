629660a182b280a8a6cbeca4fb8c38b5bd8239a7
owenbcoding <eoghankb@gmail.com>
2025-02-01 15:47:24 +0000

    changed background color to gray for tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,5 +1,5 @@
 @props(['tag', 'size' => 'small'])
 
-<span class="inline-flex items-center px-5 py-2 rounded-md text-sm font-medium bg-blue-100 text-blue-800">
+<span class="inline-flex items-center px-2 py-2 rounded-md text-sm font-medium bg-blue-100 text-white bg-gray-400">
     {{ $tag->name }}
 </span>

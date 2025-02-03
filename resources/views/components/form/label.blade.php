63857f1507f0fa758657082e317eecd858b601d2
owenbcoding <eoghankb@gmail.com>
2025-02-03 16:23:03 +0000

    renamed forms folder to form folder

---
+++ resources/views/components/form/label.blade.php
@@ -0,0 +1,5 @@
+@props(['for'])
+
+<label for="{{ $for }}" class="block text-sm font-medium text-gray-700">
+    {{ $slot }}
+</label>
---
+++ resources/views/components/form/tag.blade.php
@@ -0,0 +1,5 @@
+@props(['text'])
+
+<span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
+    {{ $text }}
+</span>

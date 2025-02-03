2dc8e9c50a8217f8003dc75f8f796f3106b8ff85
owenbcoding <eoghankb@gmail.com>
2025-02-03 15:07:40 +0000

    fixed broken x-laout to x-layout.app and added contact page content

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,12 @@
-<x-layout>
+<x-layouts.app>
     <x-header />
     <div class="container mx-auto mt-10">
-        <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Contact
+        <h1 class="text-center block font-medium text-gray-600 tracking-tight sm:text-5xl lg:text-6xl mt-10">Contact Me!
         </h1>
+        <p class="text-center text-gray-500 mt-5text-wrap mt-20 max-w-2xl mx-auto">This is a simple blog application I made to showcase my Full-Stack skills if you would like me to create you something similar for your business or online presense feel free to reach out.
+        </p>
+        <p class="text-center text-gray-500 mt-5text-wrap mt-20 max-w-2xl mx-auto">You can reach me at
+            <a href="mailto:{{ config('mail.from.address') }}">{{ config('mail.from.address') }}</a> 
+        </p>
     </div>
-</x-layout>
+</x-layouts.app>

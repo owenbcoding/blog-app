bb2f10ab3b25a5a73ccf4aab140162bba89d0fce
owenbcoding <eoghankb@gmail.com>
2025-02-06 19:39:55 +0000

    moved post management route on navigation above create post and created form for log out

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,9 +27,14 @@
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="{{ route('admin') }}" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
-                <a href="{{ route('admin.posts.create') }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Create-Post</a>
                 <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Post-managment</a>
-                <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Sign Out</a>
+                <a href="{{ route('admin.posts.create') }}" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Create-Post</a>
+                <div class="rounded-md px-3 px-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                  <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit">Log Out</button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>

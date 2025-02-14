1e517db84b38c09dff716b097a70e67b19dd8708
owenbcoding <eoghankb@gmail.com>
2025-02-14 17:09:14 +0000

    added imports for tag post string and storage also created validation for data when creating posts and added posts.edit to function edit

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Tag;
 use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminPostController extends Controller
 {
@@ -30,15 +33,35 @@ class AdminPostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            'title' => 'required|string|max:150',
+            'thumbnail' => 'required|image',
+            'tag' => 'required|string|max:20',
+            'excerpt' => 'required|string|max:150',
+            'body' => 'required|string|max:4096',
+        ]);
+        // dd(request()->all());
+        
+        $validateData['slug'] = Str::slug($validateData['title']);
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['thumbnail'] = $request->file('thumbnail')->store('thumbnails', 'public');
+        
+        $post=Post::create($validateData);
+
+        $tags = explode(',', $validateData['tag']);
+        if(count($tags) <= 4) { 
+            $tagIds = Tag::whereIn('name', $tags)->pluck('id');
+            $post->tags()->attach($tagIds);
+        }
+        return redirect()->route('admin.posts.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
-        //
+        return view('admin.posts.edit', ['post' => $post]);
     }
 
     /**

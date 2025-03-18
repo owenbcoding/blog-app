593b33a1a4a2fa3a20791527ebb100622755c7ad
owenbcoding <eoghankb@gmail.com>
2025-03-18 03:45:29 +0000

    added size and base to props

--- resources/views/components/form/tag.blade.php
+++ resources/views/components/form/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['text'])
+@props(['text', 'size' => 'base'])
 
 <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
     {{ $text }}

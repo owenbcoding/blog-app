8351b8eeb8b57c03771a0b605b3de47b8fd43e43
owenbcoding <eoghankb@gmail.com>
2025-03-18 03:41:50 +0000

    add protected fillable name for tags model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,6 +10,8 @@ class Tag extends Model
 {
     use HasFactory;
     
+    protected $fillable = ['name'];
+
     public function posts(): BelongsToMany
     {
         return $this->belongsToMany(Post::class);
